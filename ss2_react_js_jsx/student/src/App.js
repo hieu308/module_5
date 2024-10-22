ca203459473abdb0c969d33440ccd1d9f4712019
hieu308 <musicgoddn2023@gmail.com>
2024-10-22 09:50:40 +0700

    up

--- ss2_react_js_jsx/student/src/App.js
+++ ss2_react_js_jsx/student/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const students = [
+    {
+      company: "Alfreds Futterkiste",
+      contact: "Maria Anders",
+      country: "Germany",
+    },
+    {
+      company: "Centro comercial Moctezuma",
+      contact: "Francisco Chang",
+      country: "Mexico",
+    },
+    {
+      company: "Ernst Handel",
+      contact: "Roland Mendel",
+      country: "Austria",
+    },
+    {
+      company: "Island Trading",
+      contact: "Helen Bennett",
+      country: "UK",
+    },
+    {
+      company: "Laughing Bacchus Winecellars",
+      contact: "Yoshi Tannamuri",
+      country: "Canada",
+    },
+    {
+      company: "Magazzini Alimentari Riuniti",
+      contact: "Giovanni Rovelli",
+      country: "Italy",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <th>company</th>
+          <th>contact</th>
+          <th>country</th>
+        </tr>
+        <tbody>
+          {students.map((student) => (
+            <tr>
+              <td>{student.company}</td>
+              <td>{student.contact}</td>
+              <td>{student.country}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="container d-flex align-items-center text-center">
+        <div className="form-signin">
+          <form>
+            <img
+              className="mb-4"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png"
+              alt=""
+              width="72"
+              height="57"
+            />
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control email"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control password"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+            </div>
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">
+              Sign in
+            </button>
+            <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
